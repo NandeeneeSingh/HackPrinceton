91c20b8728aa5a6d5b8d65f72e8cebf4f4329308
windows <shuang04@terpmail.umd.edu>
2024-11-09 08:55:18 -0500

    error page

--- testing/app.py
+++ testing/app.py
@@ -3,14 +3,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return render_template('login.html', error = None)
 
 @app.route('/login', methods=['POST'])
 def login():
     iden = request.form.get('iden')
     coo = request.form.get('coo')
 
-    return redirect(url_for('display', iden=iden, coo=coo))
+    if (coo == "United States" or coo == "USA") and (len(iden) != 9):
+        return redirect(url_for('loginWError', error = "Invalid SSN"))
+    
+    elif (coo == "France" or coo == "france") and (len(iden) != 15):
+        return redirect(url_for('loginWError', error = "Invalid NIR"))
+
+    elif (coo == "India" or coo == "india") and (len(iden) != 12):
+        return redirect(url_for('loginWError', error = "Invalid Aadhar Number"))
+
+    elif (coo == "China" or coo == "china") and (len(iden) != 18):
+        return redirect(url_for('loginWError', error = "Invalid Identity Card Number"))
+        
+    elif (coo == "Ghana" or coo == "ghana") and (len(iden) != 9):
+        return redirect(url_for('loginWError', error = "Invalid Social Security and National Trust Number"))
+        
+    elif (coo == "Burma" or coo == "burma") and (len(iden) > 64):
+        return redirect(url_for('loginWError', error = "Invalid UID number"))
+        
+    elif (coo == "Cambodia" or coo == "cambodia") and (len(iden) != 18):
+        return redirect(url_for('loginWError', error = "Invalid Identification Number"))
+    
+    else:
+        return redirect(url_for('display', iden=iden, coo=coo))
+
+@app.route('/loginWError')
+def loginWError():
+    error = request.args.get('error')
+    return render_template('loginWError.html', error = error)
+
 
 @app.route('/display')
 def display():
