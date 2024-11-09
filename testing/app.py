dd7bb6ef64612edced9cc9b55bc69a55c5f304c0
Henry-P-Ding <ding.henry.23@gmail.com>
2024-11-09 17:33:48 -0500

    Refactor app.py

--- testing/app.py
+++ testing/app.py
@@ -2,57 +2,64 @@ from flask import Flask, redirect, url_for, request, render_template, session
 from patientData import patientData
 
 app = Flask(__name__)
-app.secret_key = 'key'
-patdata = patientData("./static/PatientsDatabase.csv").data
+app.secret_key = "key"
+patient_data = patientData("./static/PatientsDatabase.csv").data
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('login.html', error = None)
+    return render_template("login.html", error=None)
 
-@app.route('/login', methods=['POST'])
+
+@app.route("/login", methods=["POST"])
 def login():
-    iden = request.form.get('iden')
-    coo = request.form.get('coo')
+    identification_number = request.form.get("iden")
+    country_of_origin = request.form.get("coo")
 
-    if (coo == "United States" or coo == "USA") and (len(iden) != 9):
-        return redirect(url_for('loginWError', error = "Invalid SSN"))
-    
-    elif (coo == "France" or coo == "france") and (len(iden) != 15):
-        return redirect(url_for('loginWError', error = "Invalid NIR"))
-
-    elif (coo == "India" or coo == "india") and (len(iden) != 12):
-        return redirect(url_for('loginWError', error = "Invalid Aadhar Number"))
-
-    elif (coo == "China" or coo == "china") and (len(iden) != 18):
-        return redirect(url_for('loginWError', error = "Invalid Identity Card Number"))
-        
-    elif (coo == "Ghana" or coo == "ghana") and (len(iden) != 9):
-        return redirect(url_for('loginWError', error = "Invalid Social Security and National Trust Number"))
-        
-    elif (coo == "Burma" or coo == "burma") and (len(iden) > 64):
-        return redirect(url_for('loginWError', error = "Invalid UID number"))
-        
-    elif (coo == "Cambodia" or coo == "cambodia") and (len(iden) != 18):
-        return redirect(url_for('loginWError', error = "Invalid Identification Number"))
-    
+    if country_of_origin == "United States" and len(identification_number) != 9:
+        return redirect(url_for("loginWError", error="Invalid SSN"))
+    elif country_of_origin == "France" and len(identification_number) != 15:
+        return redirect(url_for("loginWError", error="Invalid NIR"))
+    elif country_of_origin == "India" and len(identification_number) != 12:
+        return redirect(url_for("loginWError", error="Invalid Aadhar Number"))
+    elif country_of_origin == "China" and len(identification_number) != 18:
+        return redirect(url_for("loginWError", error="Invalid Identity Card Number"))
+    elif country_of_origin == "Ghana" and len(identification_number) != 9:
+        return redirect(
+            url_for(
+                "loginWError", error="Invalid Social Security and National Trust Number"
+            )
+        )
+    elif country_of_origin == "Burma" and len(identification_number) > 64:
+        return redirect(url_for("loginWError", error="Invalid UID number"))
+    elif country_of_origin == "Cambodia" and len(identification_number) != 18:
+        return redirect(url_for("loginWError", error="Invalid Identification Number"))
     else:
-        return redirect(url_for('display', iden=iden, coo=coo))
+        return redirect(
+            url_for(
+                "display",
+                identification_number=identification_number,
+                country_of_origin=country_of_origin,
+            )
+        )
 
-@app.route('/loginWError')
+
+@app.route("/loginWError")
 def loginWError():
-    error = request.args.get('error')
-    return render_template('loginWError.html', error = error)
+    error = request.args.get("error")
+    return render_template("loginWError.html", error=error)
 
 
-@app.route('/display')
+@app.route("/display")
 def display():
     # Retrieve the username and password from the query parameters
-    iden = request.args.get('iden')
-    coo = request.args.get('coo')
-    print(patdata[int(iden)])
-    name = patdata[int(iden)].name
-    visits = patdata[int(iden)].visits
-    return render_template('visits.html', name = name, visits = visits)
+    identification_number = request.args.get("identification_number")
+    country_of_origin = request.args.get("country_of_origin")
+    print(patient_data[int(identification_number)])
+    name = patient_data[int(identification_number)].name
+    visits = patient_data[int(identification_number)].visits
+    return render_template("visits.html", name=name, visits=visits)
 
-if __name__ == '__main__':
-    app.run(debug = True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
